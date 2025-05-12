3acaf5599e5954706fc6ab94d1f00af4ecedc2f4
pdvr99 <49884229+pdvr99@users.noreply.github.com>
2025-05-12 18:12:55 -0400

    Update gps_parser_tb.vhd

--- gps_parser_tb.vhd
+++ gps_parser_tb.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 05/01/2025 09:28:25 AM
+-- Create Date: 04/29/2025 11:18:44 AM
 -- Design Name: 
--- Module Name: gps_parser_tb - Behavioral
+-- Module Name: gps_parser - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -24,22 +24,16 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity gps_parser_tb is
---  Port ( );
-end gps_parser_tb;
-
-architecture Behavioral of gps_parser_tb is
-
-   component gps_parser
-        port( 
-            clk : in std_logic;
+entity gps_parser is
+    port ( 
+            clk : in std_logic; 
             rst : in std_logic;
             ram_data : in std_logic_vector(7 downto 0);
             start_parse : in std_logic;  
@@ -47,144 +41,83 @@ architecture Behavioral of gps_parser_tb is
             read_addr : out integer range 0 to 127; 
             latitude_data : out std_logic_vector(71 downto 0); 
             longitude_data : out std_logic_vector(79 downto 0)
-        );
-    end component;
-    
-    type str69 is array(0 to 69) of std_logic_vector(7 downto 0);
-
-    signal gprmc_sentence : str69 := (
-        x"24", -- $
-        x"47", -- G
-        x"50", -- P
-        x"52", -- R
-        x"4D", -- M
-        x"43", -- C
-        x"2C", -- ,
-        x"31", -- 1
-        x"32", -- 2
-        x"33", -- 3
-        x"35", -- 5
-        x"31", -- 1
-        x"39", -- 9
-        x"2C", -- ,
-        x"41", -- A
-        x"2C", -- ,
-        x"34", -- 4
-        x"38", -- 8
-        x"30", -- 0
-        x"37", -- 7
-        x"2E", -- .
-        x"30", -- 0
-        x"33", -- 3
-        x"38", -- 8
-        x"2C", -- ,
-        x"4E", -- N
-        x"2C", -- ,
-        x"30", -- 0
-        x"31", -- 1
-        x"31", -- 1
-        x"33", -- 3
-        x"31", -- 1
-        x"2E", -- .
-        x"30", -- 0
-        x"30", -- 0
-        x"30", -- 0
-        x"2C", -- ,
-        x"45", -- E
-        x"2C", -- ,
-        x"30", -- 0
-        x"32", -- 2
-        x"32", -- 2
-        x"2E", -- .
-        x"34", -- 4
-        x"2C", -- ,
-        x"30", -- 0
-        x"38", -- 8
-        x"34", -- 4
-        x"2E", -- .
-        x"34", -- 4
-        x"2C", -- ,
-        x"32", -- 2
-        x"33", -- 3
-        x"30", -- 0
-        x"33", -- 3
-        x"39", -- 9
-        x"34", -- 4
-        x"2C", -- ,
-        x"30", -- 0
-        x"30", -- 0
-        x"33", -- 3
-        x"2E", -- .
-        x"31", -- 1
-        x"2C", -- ,
-        x"57", -- W
-        x"2A", -- *
-        x"36", -- 6
-        x"41", -- A
-        x"0D", -- CR
-        x"0A"  -- LF
     );
-        
-    signal clk : std_logic;
-    signal rst : std_logic := '0';
-    signal ram_data : std_logic_vector(7 downto 0) := (others => '0');
-    signal start_parse : std_logic := '0';
-    signal done : std_logic;
-    signal read_addr : integer range 0 to 127;
-    signal latitude_data : std_logic_vector(71 downto 0);
-    signal longitude_data : std_logic_vector(79 downto 0);
+end gps_parser;
+
+architecture Behavioral of gps_parser is
     
+    signal addr : integer range 0 to 127 := 0; 
+    signal comma_count : integer range 0 to 12 := 0;
+    
+    signal read_addr_inter : integer range 0 to 127 := 0; 
+    
+    signal latitude_data_inter : std_logic_vector(71 downto 0) := x"303030303030303430";
+    signal longitude_data_inter : std_logic_vector(79 downto 0) := x"30303030303030303330"; 
+    
+    signal latitude_index : integer range 0 to 8 := 0;
+    signal longitude_index : integer range 0 to 9 := 0;
 
-
+    signal counter : std_logic_vector(24 downto 0) := (others => '0');
+    
+    signal done_inter : std_logic; 
 
 begin
+    
+    process(clk)
+    begin 
+        if rising_edge(clk) then
+            if rst = '1' then 
+                comma_count <= 0; 
+                latitude_data_inter <= x"303030303030303430";
+                longitude_data_inter <= x"30303030303030303330";
+                read_addr_inter <= 0; 
+                done_inter <= '0'; 
+                counter <= (others => '0'); 
+            elsif start_parse = '1' then
+            
+                read_addr_inter <= read_addr_inter + 1; 
+                
+                if(read_addr_inter = 69) then
+                    read_addr_inter <= 0; 
+                end if; 
+                
+                if ram_data = x"2C" then --comma
+                     comma_count <= comma_count + 1; 
+                else     
+                     if comma_count = 3 and latitude_index < 9 then
+                        latitude_data_inter <= latitude_data_inter(63 downto 0) & ram_data; 
+                        latitude_index <= latitude_index + 1;
+                     elsif comma_count = 5 and longitude_index < 10 then 
+                        longitude_data_inter <= longitude_data_inter(71 downto 0) & ram_data; 
+                        longitude_index <= longitude_index + 1;
+                     end if;   
+                end if; 
+                
+                if comma_count > 5 then
+                    done_inter <= '1'; 
+                else
+                    done_inter <= '0'; 
+                end if; 
 
-
-    dut: gps_parser
-        port map (
-            clk => clk,
-            rst => rst,
-            ram_data => ram_data,
-            start_parse => start_parse,
-            done => done,
-            read_addr => read_addr,
-            latitude_data => latitude_data,
-            longitude_data => longitude_data
-        );
-
-    process
-    begin
-        clk <= '0';
-        wait for 4 ns;
-        clk <= '1';
-        wait for 4 ns;
+               
+                if(unsigned(counter) = 1) then
+                    done_inter <= '1';
+                    latitude_data_inter <= x"303030303030303430";
+                    longitude_data_inter <= x"30303030303030303330";
+                    counter <= (others => '0');
+                end if; 
+                
+                counter <= std_logic_vector(unsigned(counter)+ 1); 
+            end if;  
+        end if;
     end process;
+
+
+    done <= done_inter; 
+    read_addr <= read_addr_inter;
+    latitude_data <= latitude_data_inter;
+    longitude_data <= longitude_data_inter;
     
-    process
-    begin
-        -- Apply reset
-        rst <= '1';
-        wait for 20 ns;
-        rst <= '0';
-        wait for 20 ns;
-    
-        -- Start feeding the sentence byte by byte
-        for i in 0 to 69 loop
-            
-            start_parse <= '1'; 
-            
-            -- Send each byte of the sentence
-            ram_data <= gprmc_sentence(i);
-            
-            wait until rising_edge(clk); -- Wait for 20 ns between sending characters
-         
-        end loop;
-    
-        -- End simulation after the sentence has been sent
-        wait for 1000 ns; -- Allow time for the parser to process the sentence and assert the ready signals
-    
-    -- End simulation
-end process;
 
 
 end Behavioral;
